9a99b49ad1a83efb27638fef85cc3bbe35a969db
Lorena da Silva Azevedo <lorenaazevedo41@gmail.com>
2021-11-14 01:59:40 -0300

    adicionando validação do form de cadastro

--- src/rotas/paginaCadastro.tsx
+++ src/rotas/paginaCadastro.tsx
@@ -8,60 +8,60 @@ function Cadastro() {
                     <legend>Cadastre seu imóvel</legend>
                     <Form.Group className="mb-3" controlId="formGridAddress1">
                         <Form.Label>Título</Form.Label>
-                        <Form.Control type="text" placeholder="Título do anúncio" name="locacao_nome" required/>
+                        <Form.Control type="text" placeholder="Título do anúncio" name="locacao_nome" minLength={5} maxLength={50} required/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGridAddress1">
                         <Form.Label>Descrição longa</Form.Label>
-                        <Form.Control type="text" placeholder="Descrição longa do anúncio" name="descricao_longa" required/>
+                        <Form.Control type="text" placeholder="Descrição longa do anúncio" name="descricao_longa" minLength={5} maxLength={250} required/>
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formGridAddress1">
                         <Form.Label>Descrição curta</Form.Label>
-                        <Form.Control type="text" placeholder="Descrição curta do anúncio" name="descricao_curta" required/>
+                        <Form.Control type="text" placeholder="Descrição curta do anúncio" name="descricao_curta" minLength={5} maxLength={50} required/>
                     </Form.Group>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Cidade</Form.Label>
-                            <Form.Control type="text" placeholder="Digite a cidade" name="localidade" required/>
+                            <Form.Control type="text" placeholder="Digite a cidade" name="localidade" minLength={5} maxLength={25} required/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
                             <Form.Label>UF</Form.Label>
-                            <Form.Control type="text" placeholder="Digite a UF (unidade federativa)" name="uf" required/>
+                            <Form.Control type="text" placeholder="Digite a UF (unidade federativa)" name="uf" minLength={2} maxLength={2} required/>
                         </Form.Group>
                     </Row>
 
                     <Form.Group className="mb-3" controlId="formGridAddress1">
                         <Form.Label>Logradouro</Form.Label>
-                        <Form.Control type="text" placeholder="Digite o logradouro" name="logradouro" required/>
+                        <Form.Control type="text" placeholder="Digite o logradouro" name="logradouro" minLength={5} maxLength={50} required/>
                     </Form.Group>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Bairro</Form.Label>
-                            <Form.Control type="text" placeholder="Digite o bairro" name="bairro" required/>
+                            <Form.Control type="text" placeholder="Digite o bairro" name="bairro" minLength={5} maxLength={50} required/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
                             <Form.Label>Complemento</Form.Label>
-                            <Form.Control type="text" placeholder="Digite o complemento" name="complemento" required/>
+                            <Form.Control type="text" placeholder="Digite o complemento" name="complemento" minLength={5} maxLength={100} required/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
                             <Form.Label>CEP</Form.Label>
-                            <Form.Control type="string" placeholder="EX.: 00000-000" name="cep" required/>
+                            <Form.Control type="string" placeholder="EX.: 00000-000" name="cep" minLength={8} maxLength={8} required/>
                         </Form.Group>
                     </Row>
 
                     <Row className="mb-3">
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Valor da diária</Form.Label>
-                            <Form.Control type="string" placeholder="Digite o preço" name="preco" required/>
+                            <Form.Control type="string" placeholder="Digite o preço" name="preco" min={0} required/>
                         </Form.Group>
 
                         <Form.Group as={Col} controlId="formGridPassword">
                             <Form.Label>Capacidade</Form.Label>
-                            <Form.Control type="number" placeholder="Digite a capacidade de pessoas" name="capacidade" required/>
+                            <Form.Control type="number" placeholder="Digite a capacidade de pessoas" name="capacidade" min={1} required/>
                         </Form.Group>
                     </Row>
                 </fieldset>
