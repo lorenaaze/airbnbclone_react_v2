aa914556074d07eb1ee550abb5e88aa76f327dff
LizSantana <liz.santanabrito@gmail.com>
2021-11-14 01:57:19 -0300

    fix: busca com filtro funcionando

--- src/rotas/paginaRetornoLocacoes.tsx
+++ src/rotas/paginaRetornoLocacoes.tsx
@@ -5,7 +5,6 @@ import LocacaoCard from '../containers/PaginaOfertas/LocacaoCard/locacaoCard'
 import { InputGroup, Dropdown, Card, DropdownButton, Container, Form, Col, Row, Button, FormControl } from 'react-bootstrap';
 import { NavLink, Outlet } from 'react-router-dom';
 import { FiltroOfertas } from './paginaFiltroOfertas';
-import SearchList from '../containers/searchList';
 import Scroll from '../containers/scroll';
 
 
@@ -43,10 +42,12 @@ function RetornoLocacoes() {
     return selectedOption;
   };
 
+  console.log(searchFilter);
+
 
 const filteredLocacoes = dados?.filter(
   locacao => {
-    if(onChangeHandler.toString() === 'uf'){
+    if(searchFilter === 'uf'){
       return(
         locacao
         .uf
@@ -54,7 +55,7 @@ const filteredLocacoes = dados?.filter(
         .includes(searchField.toLowerCase())
       );
     }
-    if(onChangeHandler.toString() === 'localidade'){
+    if(searchFilter === 'localidade'){
       return(
         locacao
         .localidade
@@ -62,7 +63,7 @@ const filteredLocacoes = dados?.filter(
         .includes(searchField.toLowerCase())
       );
     }
-    if(onChangeHandler.toString() === 'capacidade'){
+    if(searchFilter === 'capacidade'){
       return(
         locacao
         .capacidade
@@ -71,7 +72,7 @@ const filteredLocacoes = dados?.filter(
         .includes(searchField.toLowerCase())
       );
     }
-    if(onChangeHandler.toString() === 'preco'){
+    if(searchFilter === 'preco'){
       return(
         locacao
         .preco
@@ -87,95 +88,28 @@ const filteredLocacoes = dados?.filter(
       .includes(searchField.toLowerCase())
     );
   });
-  
- /* const filteredLocacoes = dados?.filter(
-    locacao => {
-      return (
-        locacao
-        .locacao_nome
-        .toLowerCase()
-        .includes(searchField.toLowerCase()) ||
-        locacao
-        .uf
-        .toLowerCase()
-        .includes(searchField.toLowerCase()) ||
-        locacao
-        .localidade
-        .toLowerCase()
-        .includes(searchField.toLowerCase()) ||
-        locacao
-        .capacidade
-        .toString()
-        .toLowerCase()
-        .includes(searchField.toLowerCase()) ||
-        locacao
-        .preco
-        .toString()
-        .toLowerCase()
-        .includes(searchField.toLowerCase())
-      )
-    }
-  )*/
 
   const handleChange = (event: any) => {
     setSearchField(event.target.value);
   }
-
-  function searchList(){
-    let dadosBusca = () => {
-      if(filteredLocacoes === undefined){
-        return dados;
-      } else{
-        return filteredLocacoes;
-      }
-    }
-    return <Scroll>
-      <SearchList filteredLocacoes={dadosBusca}/>
-    </Scroll>
-  }
-
-
   return (
     <>
       <Container>
       {console.log(typeof(dados))}
       <Form>
                 <Row>
-                    <Col className="col-xl-3">
+                    <Col className="">
                         <InputGroup className="mb-3">
-                          {/* <FormControl
-                                placeholder="Busque pelo estado (UF), cidade, capacidade ou preço:"
-                                aria-label="Busque pelo estado, cidade, capacidade ou preço:"
-                                type="search" 
-                                value={searchParams.get('filtro') || ''}
-                                onChange={event => {
-                                  let filtro = event.target.value;
-                                  if (filtro) {
-                                    setSearchParams({ filtro });
-                                  } else {
-                                    setSearchParams({});
-                                  }
-                                }}
-                              />*/}
-                              <FormControl type="search" 
-                              aria-label="Busque pelo estado, cidade, capacidade ou preço:"
-                              placeholder="Busque pelo estado (UF), cidade, capacidade ou preço:"
-                              onChange={handleChange}/>
-                            <DropdownButton
-                                variant="outline-secondary"
-                                title="Filtre por:"
-                                id="dropdown-basic">
-                                <Dropdown.Item as={NavLink} to="/locacao?uf/">Estado</Dropdown.Item>
-                                <Dropdown.Item as={NavLink} to="/locacao?localidade/">Cidade</Dropdown.Item>
-                                <Dropdown.Item as={NavLink} to="/locacao?capacidade/">Capacidade</Dropdown.Item>
-                                <Dropdown.Item as={NavLink} to="/locacao?preco/">Preço</Dropdown.Item>
-                            </DropdownButton>
-                      <select onChange={onChangeHandler}>
-                        <option value="uf">Estado</option>
-                        <option value="localidade">Cidade</option>
-                        <option value="capacidade">Capacidade</option>
-                        <option value="preco">Preço</option>
-                      </select>
+                          <FormControl type="search" 
+                          aria-label="Busque pelo estado, cidade, capacidade ou preço:"
+                          placeholder="Busque pelo estado (UF), cidade, capacidade ou preço:"
+                          onChange={handleChange}/>
+                          <select onChange={onChangeHandler}>
+                            <option value="uf">Estado</option>
+                            <option value="localidade">Cidade</option>
+                            <option value="capacidade">Capacidade</option>
+                            <option value="preco">Preço</option>
+                          </select>
                         </InputGroup>
                     <Col className="col-xl-2">
                     </Col>
@@ -187,13 +121,13 @@ const filteredLocacoes = dados?.filter(
             <ul>
             <Card>
              <LocacaoCard key={filteredLocacao._id} 
-             locacao_nome={filteredLocacao.locacao_nome} 
-             uf={filteredLocacao.uf} 
-             localidade={filteredLocacao.localidade} 
-             bairro={filteredLocacao.bairro}
-              logradouro={filteredLocacao.logradouro}
-               preco={filteredLocacao.preco}
-                capacidade={filteredLocacao.capacidade}
+                          locacao_nome={filteredLocacao.locacao_nome} 
+                          uf={filteredLocacao.uf} 
+                          localidade={filteredLocacao.localidade} 
+                          bairro={filteredLocacao.bairro}
+                          logradouro={filteredLocacao.logradouro}
+                          preco={filteredLocacao.preco}
+                          capacidade={filteredLocacao.capacidade}
                  phone={filteredLocacao.proprietario.phone}/>
             </Card>
           </ul>
