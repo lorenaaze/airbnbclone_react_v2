b17f5ffd8f543d8ccd80c333201427bb81091856
Bruno Machado <brunomdr46@gmail.com>
2021-11-26 14:00:35 -0300

    Criação dos formulários de login e registro

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import PaginaInicial from './containers/PaginaInicial';
-import Cadastro from './containers/PaginaCadastro';
-import RetornoLocacoes from './containers/PaginaOfertas';
-import PaginaNaoEncontrada from './containers/PaginaNaoEncontrada';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import  RetornoLocacao  from './containers/PaginaLocacao';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PaginaInicial from "./containers/PaginaInicial";
+import Cadastro from "./containers/PaginaCadastro";
+import RetornoLocacoes from "./containers/PaginaOfertas";
+import PaginaNaoEncontrada from "./containers/PaginaNaoEncontrada";
+import "bootstrap/dist/css/bootstrap.min.css";
+import RetornoLocacao from "./containers/PaginaLocacao";
+import PaginaLogin from "./containers/PaginaLogin";
+import PaginaRegistroUsuario from "./containers/PaginaRegistroUsuario";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<PaginaInicial/>}/>
-          <Route path="locacao" element={<RetornoLocacoes/>}/>
-          <Route path="locacao/:id" element={<RetornoLocacao/>}/>
-        <Route path="cadastro" element={<Cadastro/>}/>
-        <Route path="*" element={<PaginaNaoEncontrada/>}/>
-      </Route>
+        <Route path="/" element={<App />}>
+          <Route index element={<PaginaInicial />} />
+          <Route path="locacao" element={<RetornoLocacoes />} />
+          <Route path="locacao/:id" element={<RetornoLocacao />} />
+          <Route path="cadastro" element={<Cadastro />} />
+          <Route path="login" element={<PaginaLogin />} />
+          <Route path="registrar" element={<PaginaRegistroUsuario />} />
+          <Route path="*" element={<PaginaNaoEncontrada />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
 
 reportWebVitals();
